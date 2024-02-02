1d26457d8c42bfa593ecb9766c3207cb83cf986c
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-02 09:57:43 +0000

    add shared library files

--- src/franka_block.cpp
+++ src/franka_block.cpp
@@ -17,18 +17,14 @@ class MJSimulation : public rclcpp::Node
     MJSimulation()
     : Node("mujoco_simulation"), count_(0)
     {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+      500ms, std::bind(&MJSimulation::timer_callback, this));
     }
 
   private:
     void timer_callback()
     {
-      auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+      RCLCPP_INFO(this->get_logger(), "Hello");
     }
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
@@ -38,7 +34,7 @@ class MJSimulation : public rclcpp::Node
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<MJSimulation>());
   rclcpp::shutdown();
   return 0;
 }
