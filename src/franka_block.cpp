d4c3240117ff1c546a189245cc45085c0f3d0c12
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-02 19:24:07 +0000

    update

--- src/franka_block.cpp
+++ src/franka_block.cpp
@@ -16,6 +16,7 @@
 #include <mujoco/mujoco.h>
 
 #include <GLFW/glfw3.h>
+#include <opencv2/opencv.hpp>
 
 using namespace std::chrono_literals;
 
@@ -35,12 +36,25 @@ class MJSimulation : public rclcpp::Node
 
       // publishers
       joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/mujoco_joint_states", rclcpp::SensorDataQoS());
-      camera_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/overhead_camera", rclcpp::SensorDataQoS());
+      camera_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/overhead_camera", 10);
       clock_publisher_ = this->create_publisher<rosgraph_msgs::msg::Clock>("/clock", rclcpp::SensorDataQoS());
       
+      // simulation update timer
       timer_ = this->create_wall_timer(
-      500ms, std::bind(&MJSimulation::update_sim, this));
+      10ms, std::bind(&MJSimulation::update_sim, this));
       
+      // init GLFW
+      if (!glfwInit()) {
+    	mju_error("Could not initialize GLFW");
+  	}
+      glfwWindowHint(GLFW_VISIBLE, 0);
+      glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_FALSE);
+      GLFWwindow* window = glfwCreateWindow(640, 480, "Invisible window", NULL, NULL);
+      if (!window) {
+        mju_error("Could not create GLFW window");
+      }
+      glfwMakeContextCurrent(window);
+     
       // read model from file and check for errors
       char error[1000] = "Could not load binary model";
       m = mj_loadXML("/home/peter/Code/research_projects/mujoco_debug/src/mujoco_ros_sim/models/scene.xml", 0, error, 1000);
@@ -48,26 +62,35 @@ class MJSimulation : public rclcpp::Node
       	mju_error("Could not load model");
       }
 
-      // make data
+      // setup mujoco
       d = mj_makeData(m);
-      last_camera_publish_time = 0.0;
-
-      // init GLFW
-      if (!glfwInit()) {
-    	mju_error("Could not initialize GLFW");
-  	}
-      window = glfwCreateWindow(800, 600, "Mujoco Simulation", NULL, NULL);
-      glfwMakeContextCurrent(window);
-
-      // initialize visualization data structures
+      mj_forward(m, d);
+      
       mjv_defaultCamera(&cam);
       mjv_defaultOption(&opt);
       mjv_defaultScene(&scn);
       mjr_defaultContext(&con);
-
-      // create scene and context
+      
       mjv_makeScene(m, &scn, 2000);
       mjr_makeContext(m, &con, mjFONTSCALE_150);
+      
+
+      // set rendering to offscreen buffer
+      mjr_setBuffer(mjFB_OFFSCREEN, &con);
+      if (con.currentBuffer!=mjFB_OFFSCREEN) {
+        std::printf("Warning: offscreen rendering not supported, using default/window framebuffer\n");
+      }
+
+      // get size of active renderbuffer
+      mjrRect viewport =  mjr_maxViewport(&con);
+      int W = viewport.width;
+      int H = viewport.height;
+      RCLCPP_INFO(this->get_logger(), "Width: %d, Height: %d", W, H);
+
+      // allocate rgb and depth buffers
+      rgb = (unsigned char*)std::malloc(3*W*H);
+
+      last_camera_publish_time = 0.0;
     }
 
   private:
@@ -82,7 +105,9 @@ class MJSimulation : public rclcpp::Node
 
       // publish clock and joint states
       auto message = rosgraph_msgs::msg::Clock();
-      message.clock = rclcpp::Time(d->time);
+      int64_t time = d->time * 1e9;
+      message.clock.sec = rclcpp::Time(time).seconds();
+      message.clock.nanosec = rclcpp::Time(time).nanoseconds();
       clock_publisher_->publish(message);
 
       //auto joint_state = sensor_msgs::msg::JointState();
@@ -93,13 +118,35 @@ class MJSimulation : public rclcpp::Node
 	
       // periodically publish camera images
       if (d->time - last_camera_publish_time > 1.0/30.0) {
-	last_camera_publish_time = d->time;
-	auto image = sensor_msgs::msg::Image();
-	image.header.stamp = rclcpp::Time(d->time);
+	RCLCPP_INFO(this->get_logger(), "Publishing camera image");
+
+	// render offscreen
+        mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn); 
+	mjr_render(viewport, &scn, &con);
+	mjr_readPixels(rgb, NULL, viewport, &con);
 	
-	// update scene and write image data to message
-        //mjv_updateScene(this->m, this->d, &opt, NULL, &cam, mjCAT_ALL, &scn); 
+	// try plot with opencv
+	cv::Mat img = cv::Mat(480, 640, CV_8UC3, rgb);
+	cv::imshow("image", img);
+	cv::waitKey(1);
+
+	// create image message
+	auto image = sensor_msgs::msg::Image();
+	image.header.stamp = rclcpp::Time(time);
+	image.height = 480;
+	image.width = 640;
+	image.encoding = "rgb8";
+	image.is_bigendian = 0;
+	image.step = image.width * 3;
+	size_t data_size = image.width * image.height * 3;
+	image.data.resize(data_size);
+	std::memcpy(&image.data[0], rgb, data_size);
+	
+	// publish image
 	camera_publisher_->publish(image);
+	
+	// update last publish time
+	last_camera_publish_time = d->time;
       }
 
     }
@@ -127,6 +174,8 @@ class MJSimulation : public rclcpp::Node
 
     // GLFW variables
     GLFWwindow* window;
+    unsigned char* rgb;
+    mjrRect viewport;
 };
 
 int main(int argc, char * argv[])
