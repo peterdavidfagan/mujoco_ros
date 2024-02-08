d39766d2cc7838db2d947c8c5845ffa6e1e1fd32
peterdavidfagan <peterdavidfagan@gmail.com>
2024-02-08 18:11:18 +0000

    flip opengl images

--- src/franka.cpp
+++ src/franka.cpp
@@ -28,17 +28,12 @@ class MJSimulation : public rclcpp::Node
 public:
   MJSimulation() : Node("mujoco_simulation"), count_(0)
   {
-    this->declare_parameter("model_file", "./models/franka.mjb");
     
-    // check model file
-    RCLCPP_INFO(this->get_logger(), "Loading model from file: %s", this->get_parameter("model_file").as_string().c_str());
-
     // read mujoco model from file and check for errors
-    char error[1000] = "Could not load binary model";
-    
+    this->declare_parameter("model_file", "./models/franka.mjb");
     std::string model_file_path = this->get_parameter("model_file").as_string();
     const char* filename = model_file_path.c_str();
-
+    char error[1000] = "Could not load binary model";
     if (std::strlen(filename) > 4 && !std::strcmp(filename + std::strlen(filename) - 4, ".mjb"))
     {
       m = mj_loadModel(filename, 0);
@@ -57,6 +52,8 @@ public:
     mj_forward(m, d);
     mju_copy(d->qpos, hold_qpos, 7);
 
+    
+    // outline fixed cameras you want to use
     overhead_cam.type = mjCAMERA_FIXED;
     overhead_cam.fixedcamid = 1;
     front_cam.type = mjCAMERA_FIXED;
@@ -66,7 +63,10 @@ public:
     right_cam.type = mjCAMERA_FIXED;
     right_cam.fixedcamid = 4;
 
-    // configure ROS communication
+    
+    // setup ROS 2 communication
+    
+    // quality of service and callback groups
     static const rmw_qos_profile_t rmw_qos_profile_reliable = { RMW_QOS_POLICY_HISTORY_KEEP_LAST,
                                                                 10,
                                                                 RMW_QOS_POLICY_RELIABILITY_RELIABLE,
@@ -77,12 +77,9 @@ public:
                                                                 RMW_QOS_LIVELINESS_LEASE_DURATION_DEFAULT,
                                                                 false };
 
-    // quality of service and callback groups
     auto qos_reliable =
         rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_reliable), rmw_qos_profile_reliable);
-
     physics_step_callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
-
     render_callback_group_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
 
     // subscriptions
@@ -95,7 +92,6 @@ public:
     physics_publisher_options.callback_group = physics_step_callback_group_;
     joint_state_publisher_ = this->create_publisher<sensor_msgs::msg::JointState>("/mujoco_joint_states", qos_reliable,
                                                                                   physics_publisher_options);
-
     rclcpp::PublisherOptions camera_publisher_options;
     camera_publisher_options.callback_group = render_callback_group_;
     overhead_camera_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/overhead_camera", 10);
@@ -103,19 +99,20 @@ public:
     left_camera_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/left_camera", 10);
     right_camera_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("/right_camera", 10);
 
-    // simulation stepping timer
-    timer_ = this->create_wall_timer(10ms, std::bind(&MJSimulation::update_sim, this), physics_step_callback_group_);
+    // setup physics timestepping (control is applied at 100Hz for now)
+    timer_ = this->create_wall_timer(10ms, std::bind(&MJSimulation::step, this), physics_step_callback_group_);
 
-    // start a separate thread for rendering that runs at 10Hz
+    // setup rendering thread
     m_render = mj_copyModel(m_render, m);
     d_render = mj_copyData(d_render, m, d);
     this->start_render_thread();
   }
 
 private:
-  void update_sim()
+  void step()
   {
-    std::unique_lock<std::mutex> lock(render_mutex);
+    // apply control and step simulation
+    std::unique_lock<std::mutex> lock(physics_data_mutex);
     if (hold)
     {
       d->ctrl = hold_qpos;
@@ -126,10 +123,8 @@ private:
     }
     lock.unlock();
 
-
-    rclcpp::Time current_time = this->now();
-
     // publish joint state of franka emika panda
+    rclcpp::Time current_time = this->now();
     auto joint_state = sensor_msgs::msg::JointState();
     joint_state.header.stamp = current_time;
     for (int i = 0; i < 8; i++)
@@ -169,7 +164,8 @@ private:
       viewport = mjr_maxViewport(&con);
       int W = viewport.width;
       int H = viewport.height;
-
+	
+      // allocate buffers for rendering
       overhead_rgb = (unsigned char*)std::malloc(3 * W * H);
       front_rgb = (unsigned char*)std::malloc(3 * W * H);
       left_rgb = (unsigned char*)std::malloc(3 * W * H);
@@ -178,92 +174,56 @@ private:
       while (running_)
       {
         this->render();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
       }
     });
+  }
+  
+  void publish_single_camera(mjvCamera* cam, unsigned char* rgb, rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher)
+  {
+
+    // update scene and render
+    mjv_updateScene(m_render, d_render, &opt, NULL, cam, mjCAT_ALL, &scn);
+    mjr_render(viewport, &scn, &con);
+    mjr_readPixels(rgb, NULL, viewport, &con);
+    
+    // OpenGL uses bottom-left origin, so we need to flip the image
+    cv::Mat image_mat(viewport.height, viewport.width, CV_8UC3, rgb);
+    cv::Mat flipped_image_mat;
+    cv::flip(image_mat, flipped_image_mat, 0);
+
+    // create image message
+    auto image_msg = sensor_msgs::msg::Image();
+    image_msg.header.stamp = this->now();
+    image_msg.height = viewport.height;
+    image_msg.width = viewport.width;
+    image_msg.encoding = "rgb8";
+    image_msg.is_bigendian = false;
+    image_msg.step = 3 * viewport.width;
+    size_t data_size = 3 * viewport.width * viewport.height;
+    image_msg.data.resize(data_size);
+    std::memcpy(image_msg.data.data(), flipped_image_mat.data, data_size);
+
+    // publish image
+    publisher->publish(image_msg);
   }
 
   void render()
   {
     // copy data from sim to render buffer
-    std::unique_lock<std::mutex> lock(render_mutex);
+    std::unique_lock<std::mutex> lock(physics_data_mutex);
     d_render = mj_copyData(d_render, m, d);
     lock.unlock();
-    rclcpp::Time current_time = this->now();
-
-    // TODO: move to utility function
-    // render camera images
-    mjv_updateScene(m_render, d_render, &opt, NULL, &overhead_cam, mjCAT_ALL, &scn);
-    mjr_render(viewport, &scn, &con);
-    mjr_readPixels(overhead_rgb, NULL, viewport, &con);
-
-    mjv_updateScene(m_render, d_render, &opt, NULL, &front_cam, mjCAT_ALL, &scn);
-    mjr_render(viewport, &scn, &con);
-    mjr_readPixels(front_rgb, NULL, viewport, &con);
-
-    mjv_updateScene(m_render, d_render, &opt, NULL, &left_cam, mjCAT_ALL, &scn);
-    mjr_render(viewport, &scn, &con);
-    mjr_readPixels(left_rgb, NULL, viewport, &con);
-
-    mjv_updateScene(m_render, d_render, &opt, NULL, &right_cam, mjCAT_ALL, &scn);
-    mjr_render(viewport, &scn, &con);
-    mjr_readPixels(right_rgb, NULL, viewport, &con);
-
-    // create messages
-    auto overhead_image = sensor_msgs::msg::Image();
-    overhead_image.header.stamp = current_time;
-    overhead_image.height = 480;
-    overhead_image.width = 640;
-    overhead_image.encoding = "rgb8";
-    overhead_image.is_bigendian = 0;
-    overhead_image.step = overhead_image.width * 3;
-    size_t overhead_data_size = overhead_image.width * overhead_image.height * 3;
-    overhead_image.data.resize(overhead_data_size);
-    std::memcpy(&overhead_image.data[0], overhead_rgb, overhead_data_size);
-
-    auto front_image = sensor_msgs::msg::Image();
-    front_image.header.stamp = current_time;
-    front_image.height = 480;
-    front_image.width = 640;
-    front_image.encoding = "rgb8";
-    front_image.is_bigendian = 0;
-    front_image.step = front_image.width * 3;
-    size_t front_data_size = front_image.width * front_image.height * 3;
-    front_image.data.resize(front_data_size);
-    std::memcpy(&front_image.data[0], front_rgb, front_data_size);
-
-    auto left_image = sensor_msgs::msg::Image();
-    left_image.header.stamp = current_time;
-    left_image.height = 480;
-    left_image.width = 640;
-    left_image.encoding = "rgb8";
-    left_image.is_bigendian = 0;
-    left_image.step = left_image.width * 3;
-    size_t left_data_size = left_image.width * left_image.height * 3;
-    left_image.data.resize(left_data_size);
-    std::memcpy(&left_image.data[0], left_rgb, left_data_size);
-
-    auto right_image = sensor_msgs::msg::Image();
-    right_image.header.stamp = current_time;
-    right_image.height = 480;
-    right_image.width = 640;
-    right_image.encoding = "rgb8";
-    right_image.is_bigendian = 0;
-    right_image.step = right_image.width * 3;
-    size_t right_data_size = right_image.width * right_image.height * 3;
-    right_image.data.resize(right_data_size);
-    std::memcpy(&right_image.data[0], right_rgb, right_data_size);
 
     // publish messages
-    overhead_camera_publisher_->publish(overhead_image);
-    front_camera_publisher_->publish(front_image);
-    left_camera_publisher_->publish(left_image);
-    right_camera_publisher_->publish(right_image);
+    this->publish_single_camera(&front_cam, front_rgb, front_camera_publisher_);
+    this->publish_single_camera(&left_cam, left_rgb, left_camera_publisher_);
+    this->publish_single_camera(&right_cam, right_rgb, right_camera_publisher_);
+    this->publish_single_camera(&overhead_cam, overhead_rgb, overhead_camera_publisher_);
   }
 
   void joint_command_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
   {
-    std::unique_lock<std::mutex> lock(render_mutex);
+    std::unique_lock<std::mutex> lock(physics_data_mutex);
     for (int i = 0; i < 8; i++)
     {
       d->ctrl[i] = msg->position[i];
@@ -274,39 +234,39 @@ private:
 
   // ROS 2
   rclcpp::TimerBase::SharedPtr timer_;
-
   rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr joint_command_subscription_;
-
   rclcpp::Publisher<rosgraph_msgs::msg::Clock>::SharedPtr clock_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_state_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr overhead_camera_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr front_camera_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr left_camera_publisher_;
   rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr right_camera_publisher_;
-
   rclcpp::CallbackGroup::SharedPtr physics_step_callback_group_;
   rclcpp::CallbackGroup::SharedPtr render_callback_group_;
-
   size_t count_;
-  std::mutex render_mutex;
-  std::thread render_thread_;
+  
+  // simulation status
   bool running_ = true;
   bool hold = true;
+  mjtNum* hold_qpos = new mjtNum[8]{ 0, -0.785, 0, -2.356, 0, 1.571, 0.785, 0 };
+  
+  // memory management
+  std::mutex physics_data_mutex;
+  std::thread render_thread_;
 
   // MuJoCo variables
-  mjModel* m;              // MuJoCo model
-  mjData* d;               // MuJoCo data
-  mjModel* m_render;       // MuJoCo model for rendering
-  mjData* d_render;        // MuJoCo data for rendering
-  mjvCamera overhead_cam;  // abstract camera
-  mjvCamera front_cam;     // abstract camera
-  mjvCamera left_cam;      // abstract camera
-  mjvCamera right_cam;     // abstract camera
-  mjvOption opt;           // visualization options
-  mjvScene scn;            // abstract scene
-  mjrContext con;          // custom GPU context
+  mjModel* m;              
+  mjData* d;               
+  mjModel* m_render;       
+  mjData* d_render;        
+  mjvCamera overhead_cam;  
+  mjvCamera front_cam;     
+  mjvCamera left_cam;      
+  mjvCamera right_cam;    
+  mjvOption opt;           
+  mjvScene scn;            
+  mjrContext con;          
   mjrRect viewport;
-  mjtNum* hold_qpos = new mjtNum[8]{ 0, -0.785, 0, -2.356, 0, 1.571, 0.785, 0 };
 
   // GLFW variables
   GLFWwindow* window;
